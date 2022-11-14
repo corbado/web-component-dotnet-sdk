1d1866678e8c2bbd349713e985870039d1c2521d
Nicolai Stein <nicolai_stein@outlook.de>
2022-11-14 18:34:00 +0100

    Imports

--- CorbadoLibrary/AuthData.cs
+++ CorbadoLibrary/AuthData.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace CorbadoLibrary
+﻿namespace CorbadoLibrary
 {
     public class AuthData
     {
